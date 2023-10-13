e7f6b60ef04fdfa39886b4437f2fb8273506a856
Raj Dhakate <engrdhakate@gmail.com>
2023-10-14 00:51:28 +0530

    + about changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className="about">
       <div>
         <p className="font-gabarito white">
-          Experienced Native iOS developer. Worked on Objective-C and Swift.
+          Experienced Native iOS developer. Worked on Objective-C and Swift,
           UIKit and SwiftUI. Also have experience in cross platform development
           like React-Native. Hands on demos on Flutter and React JS too.
         </p>
